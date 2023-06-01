d9817cc31d8d67e79f6ab34c145b8a95effd1dc4
uncannybat <125538457+uncannybat@users.noreply.github.com>
2023-06-01 12:50:16 +0500

    Update webscrapper.py

--- webscrapper.py
+++ webscrapper.py
@@ -2,22 +2,22 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_anchors(url):
-   
+def scrape_anchor_titles(url):
+    
     response = requests.get(url)
     content = response.content
 
-    
+   
     soup = BeautifulSoup(content, "html.parser")
 
-    
+   
     anchor_tags = soup.find_all("a")
-    hrefs = [tag.get("href") for tag in anchor_tags]
+    titles = [tag.get("title") for tag in anchor_tags if tag.get("title")]
 
-    return hrefs
+    return titles
 
 
-st.title("Anchor Tag Web Scraper")
+st.title("Anchor Tag Title Scraper")
 
 
 url = st.text_input("Enter a URL to scrape")
@@ -26,11 +26,11 @@ url = st.text_input("Enter a URL to scrape")
 if st.button("Scrape"):
     if url:
        
-        scraped_hrefs = scrape_anchors(url)
+        scraped_titles = scrape_anchor_titles(url)
 
        
-        st.write("Scraped Anchor Tags:")
-        for href in scraped_hrefs:
-            st.write(href)
+        st.write("Scraped Titles:")
+        for title in scraped_titles:
+            st.write(title)
     else:
         st.write("Please enter a URL to scrape")
