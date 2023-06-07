cca875116485b32554e439358ebd9ad8d43c6ed2
uncannybat <125538457+uncannybat@users.noreply.github.com>
2023-06-08 00:46:11 +0500

    scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,1 +1,36 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
 
+def scrape_anchor_titles(url):
+    
+    response = requests.get(url)
+    content = response.content
+
+   
+    soup = BeautifulSoup(content, "html.parser")
+
+   
+    anchor_tags = soup.find_all("a")
+    titles = [tag.get("title") for tag in anchor_tags if tag.get("title")]
+
+    return titles
+
+
+st.title("Anchor Tag Title Scraper")
+
+
+url = st.text_input("Enter a URL to scrape")
+
+
+if st.button("Scrape"):
+    if url:
+       
+        scraped_titles = scrape_anchor_titles(url)
+
+       
+        st.write("Scraped Titles:")
+        for title in scraped_titles:
+            st.write(title)
+    else:
+        st.write("Please enter a URL to scrape")
